d568df391a96d66dca0369997ed1799725dc1427
Carlos Anriquez <canriquez@gmail.com>
2020-06-26 16:45:50 -0300

    initial core concepts config

---
+++ src/index.js
@@ -0,0 +1,6 @@
+console.log("HELLO FROM WEBPACK ACADEMY CARLOS!");
+import fooString from "./foo";
+import bar from "./bar-cjs";
+import "./preview.jpg";
+
+console.log(fooString, bar);
---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require("path");
+const ExamplePlugin = require("./ExamplePlugin.js");
+const webpack = require("webpack");
+
+module.exports = {
+    entry: "./src/index.js",
+    output: {
+        filename: "bundle.js",
+        path: path.join(__dirname, "dist")
+
+    },
+    module: {
+        rules: [
+            {
+                test: /\.jpe?g$/,
+                use: [
+                    "file-loader"
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new ExamplePlugin()
+    ]
+};
